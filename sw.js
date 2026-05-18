484a44544cce1dcfd887cfee4db00c420144a99b
dmc4nero7482-commits <dmc4nero7482@gmail.com>
2026-05-18 10:59:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'scoreboard-v1';
+const STATIC = ['./manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll([...STATIC, './scoreboard.html']))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    const url = new URL(e.request.url);
+
+    if (url.pathname.endsWith('.html') || url.pathname.endsWith('/')) {
+        e.respondWith(
+            fetch(e.request)
+                .then(res => {
+                    caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+                    return res;
+                })
+                .catch(() => caches.match(e.request))
+        );
+        return;
+    }
+
+    e.respondWith(
+        caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+});
